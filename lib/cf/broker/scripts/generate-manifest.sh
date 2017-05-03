9b8558aa3a61b71d005795246d156b604faa81b1
Nikolay Maslarski <nikolay.maslarski@sap.com>
2017-05-03 11:56:24 +0300

    Remove unused scripts
    Signed-off-by: Martin Aleksandrov <martin.aleksandrov@sap.com>

--- lib/cf/broker/scripts/generate-manifest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp manifest-template.yml manifest.yml
-echo "    CF_API: $CF_API" >> manifest.yml
-echo "    UAA_SERVER: $UAA_SERVER" >> manifest.yml
-echo "    PROVISIONING: $PROVISIONING" >> manifest.yml
-echo "    COLLECTOR: $COLLECTOR" >> manifest.yml
-echo "    BROKER_USER: $BROKER_USER" >> manifest.yml
-echo "    BROKER_PASSWORD: $BROKER_PASSWORD" >> manifest.yml
