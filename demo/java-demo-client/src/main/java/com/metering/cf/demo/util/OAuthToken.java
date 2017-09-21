9430ec9e1e624734acf9a6f104ae82bb6b308480
Hristo Iliev <hsiliev@gmail.com>
2017-09-21 22:55:40 +0300

    Fix Codacy issues in Java demo app

--- demo/java-demo-client/src/main/java/com/metering/cf/demo/util/OAuthToken.java
+++ demo/java-demo-client/src/main/java/com/metering/cf/demo/util/OAuthToken.java
@@ -7,69 +7,69 @@ import org.slf4j.LoggerFactory;
 
 public class OAuthToken {
 
-	// --------------------------------------------------------------------------------
-	// Logger
-	// --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Logger
+  // --------------------------------------------------------------------------------
 
-	private static final Logger logger = LoggerFactory.getLogger(OAuthToken.class);
+  private static final Logger logger = LoggerFactory.getLogger(OAuthToken.class);
 
-	// --------------------------------------------------------------------------------
-	// Attributes
-	// --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Attributes
+  // --------------------------------------------------------------------------------
 
-	protected String tokenString = null;
-	protected long tokenValidity = -1;
+  protected String tokenString = null;
+  protected long tokenValidity = -1;
 
-	// --------------------------------------------------------------------------------
-	// Constructor
-	// --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------------------------------
 
-	/**
-	 * OAuthToken constructor
-	 *
-	 * The created object will store
-	 *  - tokenString: The given token as-is as a string.
-	 *  - tokenValidity: The point in time (in milliseconds since January 1, 1970, 00:00:00 GMT) when the token turns invalid.
-	 *
-	 * Note: Value of tokenValidity is calculated by adding the parameter expiresIn to the current time,
-	 *       and then subtracting a buffer of 60000 milliseconds (1 minute).
-	 *
-	 * @param token Token itself as a String
-	 * @param expiresIn Time in milliseconds from now, when the token will expire
-	 */
-	public OAuthToken(String tokenString, long expiresIn){
-		this.setTokenString(tokenString);
-		this.setTokenValidity(new Date().getTime() + expiresIn - 60000);
-	}
+  /**
+   * OAuthToken constructor
+   * <p>
+   * The created object will store
+   * - tokenString: The given token as-is as a string.
+   * - tokenValidity: The point in time (in milliseconds since January 1, 1970, 00:00:00 GMT) when the token turns invalid.
+   * <p>
+   * Note: Value of tokenValidity is calculated by adding the parameter expiresIn to the current time,
+   * and then subtracting a buffer of 60000 milliseconds (1 minute).
+   *
+   * @param token     Token itself as a String
+   * @param expiresIn Time in milliseconds from now, when the token will expire
+   */
+  public OAuthToken(String tokenString, long expiresIn) {
+    this.setTokenString(tokenString);
+    this.setTokenValidity(new Date().getTime() + expiresIn - 60000);
+  }
 
-	// --------------------------------------------------------------------------------
-	// Getters / Setters
-	// --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Getters / Setters
+  // --------------------------------------------------------------------------------
 
-	public String getTokenString(){
-		return this.tokenString;
-	}
+  public String getTokenString() {
+    return this.tokenString;
+  }
 
-	public void setTokenString(String tokenString){
-		this.tokenString = tokenString;
-	}
+  public void setTokenString(String tokenString) {
+    this.tokenString = tokenString;
+  }
 
-	public long getTokenValidity(){
-		return this.tokenValidity;
-	}
+  public long getTokenValidity() {
+    return this.tokenValidity;
+  }
 
-	public void setTokenValidity(long tokenValidity) {
-		this.tokenValidity = tokenValidity;
-	}
+  public void setTokenValidity(long tokenValidity) {
+    this.tokenValidity = tokenValidity;
+  }
 
-	// --------------------------------------------------------------------------------
-	// Functions / Methods
-	// --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+  // Functions / Methods
+  // --------------------------------------------------------------------------------
 
-	public boolean isValid() {
-		boolean result = (new Date().getTime() - this.tokenValidity > 0);
-		logger.debug("OAuthToken.isValid(): " + result);
-		return result;
-	}
+  public boolean isValid() {
+    boolean result = (new Date().getTime() - this.tokenValidity > 0);
+    logger.debug("OAuthToken.isValid(): " + result);
+    return result;
+  }
 
 }
