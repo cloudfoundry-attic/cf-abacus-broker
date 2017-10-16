ea59346ac4b48b8ec2e881fe1fd89f9ed94db9ec
Georgi Lozev <georgi.lozev@sap.com>
2017-10-16 09:45:42 +0300

    Refactorings in broker smoke test
    Signed-off-by: Denica Madzhirova <denica.madzhirova@sap.com>

--- test/broker/src/test/broker-smoke-test.js
+++ test/broker/src/test/broker-smoke-test.js
@@ -11,6 +11,7 @@ const first = _.first;
 const last = _.last;
 
 const testHelper = require('abacus-ext-test-utils');
+const yieldable = require('abacus-yieldable');
 const appUtils = require('abacus-ext-test-app-utils')();
 
 const TestApp = appUtils.App;
@@ -34,52 +35,42 @@ describe('Abacus Broker Smoke test', () => {
   const app = TestApp();
   const standardInstance = TestService();
 
-  const now = moment.utc().valueOf();
-  const resourceInstanceId = `${now}-151-413-121-110987654321d`;
-  const consumerId = `app:${resourceInstanceId}`;
-  const orgId = app.orgGuid();
-  const spaceId = app.spaceGuid();
-  const expectedQuantity = 512;
-
   before(() => app.deploy());
 
-  it('should successfully create standard service instance', () => {
-    const createResult = standardInstance.create().trim();
-    expect(createResult.endsWith('OK')).to.equal(true);
+  it('should validate standard metering service instance',
+    yieldable.functioncb(function *() {
+      const createResult = standardInstance.create().trim();
+      expect(createResult.endsWith('OK')).to.equal(true,
+        'service instance was not created successfully');
 
-    const status = standardInstance.status();
-    expect(status).to.equal('create succeeded');
-  });
+      const status = standardInstance.status();
+      expect(status).to.equal('create succeeded',
+        'service status was not create succeeded');
 
-  context('when test app tries to use the instance', () => {
-    let resourceId;
-    let usageToken;
-
-    before(() => {
       standardInstance.bind(app.appName);
       app.restart();
-    });
 
-    it('should set proper variables in the application environment', (done) => {
-      app.getCredentials((err, credentials) => {
-        expect(credentials).to.have.property('client_id');
-        const clientId = credentials.client_id;
-        expect(credentials).to.have.property('client_secret');
-        const clientSecret = credentials.client_secret;
-        expect(credentials).to.have.property('resource_id');
-        resourceId = credentials.resource_id;
-        expect(credentials.collector_url).to.contain('abacus-usage-collector');
+      const credentials = yield yieldable(app.getCredentials);
+      expect(credentials).to.have.property('client_id');
+      expect(credentials).to.have.property('client_secret');
+      expect(credentials).to.have.property('resource_id');
+      expect(credentials).to.have.property('plans');
 
-        usageToken = oauth.cache(testEnv.api, clientId, clientSecret,
-          `abacus.usage.${resourceId}.write,abacus.usage.${resourceId}.read`);
-        usageToken.start((err) => {
-          expect(err).to.equal(undefined);
-          done();
-        });
-      });
-    });
+      const resourceId = credentials.resource_id;
+      const clientId = credentials.client_id;
+      const clientSecret = credentials.client_secret;
 
-    it('should successfully submit usage document', (done) => {
+      const usageToken = oauth.cache(testEnv.api, clientId, clientSecret,
+        `abacus.usage.${resourceId}.write,abacus.usage.${resourceId}.read`);
+      yield yieldable(usageToken.start);
+
+      const now = moment.utc().valueOf();
+      const resourceInstanceId = `${now}-151-413-121-110987654321d`;
+      const consumerId = `app:${resourceInstanceId}`;
+      const orgId = app.orgGuid();
+      const spaceId = app.spaceGuid();
+      const expectedQuantity = 512;
+
       const usageBody = {
         start: now,
         end: now,
@@ -95,35 +86,38 @@ describe('Abacus Broker Smoke test', () => {
         }]
       };
 
-      app.postUsage(usageBody, (err, val) => {
-        expect(err).to.equal(undefined);
-        expect(val.statusCode).to.be.oneOf([201, 409]);
-        done();
-      });
-    });
+      const postResponse = yield yieldable(app.postUsage)(usageBody);
+      expect(postResponse.statusCode).to.be.oneOf([201, 409],
+        'usage was not submitted successfully');
 
-    it('should successfully get usage with resource specific token', (done) => {
-      abacusUtils.getOrganizationUsage(usageToken, orgId,
-        (err, response) => {
-          expect(err).to.equal(undefined);
-          expect(response.statusCode).to.equal(200);
-          expect(response.body.resources.length).to.equal(1);
+      const getResponse =
+      yield yieldable(abacusUtils.getOrganizationUsage)(usageToken, orgId);
+      expect(getResponse.statusCode).to.equal(200,
+        'usage was not retrieved successfully');
+      expect(getResponse.body.resources.length).to.equal(1,
+        'number of resources was not the expected');
 
-          const expectedSpace = findWhere(
-            response.body.spaces, { space_id: spaceId });
-          const expectedConsumer = findWhere(
-            expectedSpace.consumers, { consumer_id: consumerId });
-          const expectedResources = findWhere(
-            response.body.resources, { resource_id: resourceId });
-          expect(expectedResources.resource_id).to.equal(resourceId);
-          expect(expectedSpace.resources.length).to.equal(1);
-          expect(expectedConsumer.resources.length).to.equal(1);
+      const expectedSpace = findWhere(
+        getResponse.body.spaces, { space_id: spaceId });
+      expect(expectedSpace.resources.length).to.equal(1,
+        'number of spaces was not the expected');
 
-          const monthlyQty = first(last(first(first(expectedResources.plans)
-            .aggregated_usage).windows)).quantity;
-          expect(monthlyQty).to.equal(expectedQuantity);
-          done();
-        });
-    });
-  });
-}).timeout(totalTimeout);
+      const expectedConsumer = findWhere(
+        expectedSpace.consumers, { consumer_id: consumerId });
+      expect(expectedConsumer.resources.length).to.equal(1,
+        'number of consumers was not the expected');
+
+      const expectedResources = findWhere(
+        getResponse.body.resources, { resource_id: resourceId });
+      expect(expectedResources.resource_id).to.equal(resourceId,
+        'resource provider was different than expected');
+
+      const monthlyQty = first(last(first(first(expectedResources.plans)
+        .aggregated_usage).windows)).quantity;
+      expect(monthlyQty).to.equal(expectedQuantity,
+        'monthly quantity was not the expected one');
+
+      standardInstance.unbind(app.appName);
+      standardInstance.destroy();
+    })).timeout(totalTimeout);
+});
